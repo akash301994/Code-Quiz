dd40220506533bdd47a0f303257456781e6c797f
akash301994 <patel.akash30@icloud.com>
2023-05-16 14:07:39 -0400

    added questions variable in js

---
+++ assets/script.js
@@ -0,0 +1,39 @@
+const questions = [
+    {
+        question: "Which green is usually found in a Caesar salad",
+        answers: [
+            {text: "spinach", correct: false},
+            {text: "romaine", correct: true},
+            {text: "asparagus", correct: false},
+            {text: "mint", correct: false},
+        ]
+    },
+    {
+        question: "Ray Charles was famous for playing which instrument?",
+        answers: [
+            {text: "violen", correct: false},
+            {text: "drums", correct: false},
+            {text: "guitar", correct: false},
+            {text: "piano", correct: true},
+        ]
+        
+    },
+    {
+        question: "Which country invented tea?",
+        answers: [
+            {text: "India", correct: false},
+            {text: "China", correct: true},
+            {text: "America", correct: false},
+            {text: "Japan", correct: false},
+        ]
+    },
+    {
+        question: "how many wisdom teeth does the average adult have?",
+        answers: [
+            {text: "4", correct: true},
+            {text: "5", correct: false},
+            {text: "1", correct: false},
+            {text: "none", correct: false},
+        ]
+    }
+]/
